9de7353d677e197bbcb5132a4066199a460c2855
Csizmadia Márk <airamek@teamorange.hu>
2024-09-04 09:59:41 +0200

    Add project files.

---
+++ SZGYA-2024-09-04-Utazasok/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SZGYA_2024_09_04_Utazasok
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
