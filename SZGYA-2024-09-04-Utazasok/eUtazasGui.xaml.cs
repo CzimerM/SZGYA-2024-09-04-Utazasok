013626249363e785c98041b9f7966d30edd00d2e
Csizmadia Márk <kindmysticaltree@gmail.com>
2024-09-11 11:58:53 +0200

    wpf kész

--- SZGYA-2024-09-04-Utazasok/eUtazasGui.xaml.cs
+++ SZGYA-2024-09-04-Utazasok/eUtazasGui.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,26 @@ namespace SZGYA_2024_09_04_Utazasok
     /// </summary>
     public partial class eUtazasGui : Window
     {
+        public static readonly string[] Tipusok = { "FEB", "TAB", "NYB", "NYP", "RVS", "GYK" };
+
         public eUtazasGui()
         {
             InitializeComponent();
+
+            //sorszám init
+            cmbxSorszam.Items.Add("Válasszon megállót!");
+            for (int i = 0; i <= 29; i++) cmbxSorszam.Items.Add($"{i}");
+            cmbxSorszam.SelectedIndex = 0;
+
+            //datepicker init
+            dtpckrFelszallas.SelectedDate = DateTime.Now;
+
+            //bérlettípus init
+            cmbxBerletTipus.Items.Add("Válasszon típust!");
+            foreach (var tipus in Tipusok) cmbxBerletTipus.Items.Add(tipus);
+            cmbxBerletTipus.SelectedIndex = 0;
+
+            txbKartyaAzon.MaxLength = 7;
         }
 
         private void rbtnJegyShow(object sender, RoutedEventArgs e)
@@ -33,6 +51,72 @@ namespace SZGYA_2024_09_04_Utazasok
         {
             grpbxJegy.Visibility = Visibility.Collapsed;
             grpbxBerlet.Visibility = Visibility.Visible;
+        }
+
+        private void txbKartyaAzon_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblAzonDb.Content = $"{txbKartyaAzon.Text.Length}db";
+        }
+
+        private void sldrJegyDb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblJegyDbDisp.Content = $"{sldrJegyDb.Value}db";
+        }
+
+        private void btnAdatokEll_Click(object sender, RoutedEventArgs e)
+        {
+            //megálló
+            if (cmbxSorszam.SelectedIndex == 0)
+            {
+                MessageBox.Show("Nem választott megállót!", "Hiba!", MessageBoxButton.OK);
+                return;
+            }
+            //felszállás idő
+            bool felszallasIdoError = false;
+            int ora = 0;
+            int perc = 0;
+            if (txbFelszallasIdo.Text.Length != 5) felszallasIdoError = true;
+            else if (txbFelszallasIdo.Text[2] != ':') felszallasIdoError |= true;
+            else if (!int.TryParse(txbFelszallasIdo.Text.Substring(0, 2), out ora)) felszallasIdoError = true;
+            else if (ora < 0 || ora > 24) felszallasIdoError = true;
+            else if (!int.TryParse(txbFelszallasIdo.Text.Substring(3, 2), out perc)) felszallasIdoError = true;
+            else if (perc < 0 || perc > 60) felszallasIdoError = true;
+            if (felszallasIdoError)
+            {
+                MessageBox.Show("Nem megfelelő a bemeneti karakterlánc formátuma.", "Hiba!", MessageBoxButton.OK);
+                return;
+            }
+            //kártyaazonosító
+            int azon;
+            if (txbKartyaAzon.Text.Length != 7)
+            {
+                MessageBox.Show("A kártya azonosítója nem hét karakter hosszú!", "Hiba!", MessageBoxButton.OK);
+                return;
+            }
+            else if (!int.TryParse(txbKartyaAzon.Text, out azon))
+            {
+                MessageBox.Show("A kártya azonosítója nem pozitív egész szám", "Hiba!", MessageBoxButton.OK);
+                return;
+            }
+
+            //bérlet típus
+            if (cmbxBerletTipus.SelectedIndex == 0)
+            {
+                MessageBox.Show("Nem adta meg a bérlet típusát!", "Hiba!", MessageBoxButton.OK);
+                return;
+            }
+            //bérlet érvényesség
+            if (dtpckrBerletErvenyesseg.SelectedDate == null)
+            {
+                MessageBox.Show("Nem adta meg a bérlet érvényességi idejét!", "Hiba!", MessageBoxButton.OK);
+                return;
+            }
+
+            StreamWriter sw = new StreamWriter("../../../src/utasadat.txt", true, Encoding.UTF8);
+
+            sw.WriteLine($"{cmbxSorszam.SelectedItem} {dtpckrFelszallas.SelectedDate.Value.ToString("yyyyMMdd")}-{ora.ToString("00")}{perc.ToString("00")} {txbKartyaAzon.Text} {(grpbxJegy.Visibility == Visibility.Visible ? "JGY" : cmbxBerletTipus.SelectedItem)} {(grpbxJegy.Visibility == Visibility.Visible ? sldrJegyDb.Value : dtpckrBerletErvenyesseg.SelectedDate.Value.ToString("yyyyMMdd"))}");
+
+            sw.Close();
         }
     }
 }
